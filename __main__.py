18293f830ac2ab1a0a18381d3bfece4f977f9fdd
RodrigoKenji <digo.kenji@hotmail.com>
2021-08-07 18:15:40 -0300

    created face reco

--- __main__.py
+++ __main__.py
@@ -1,14 +1,35 @@
 import numpy as np
 import face_recognition as fr
 import cv2
+import json
+import os, os.path
+from os import listdir
+from os.path import isfile, join
+
+def collect_imgs(path, known_face, known_names):
+
+    profiles = [f for f in listdir(path) if isfile(join(path, f))]
+    profiles_ln = len([name for name in os.listdir(path) if os.path.isfile(os.path.join(path, name))])
+    profiles_list = []
+    images = []
+
+    for i in range(profiles_ln):
+        with open('Profiles/' + profiles[i]) as f:
+            profiles_list.append(json.load(f))
+
+    for x in range(profiles_ln):
+        images.append(fr.load_image_file("Images/"+profiles_list[x]['img']))
+        known_face.append(fr.face_encodings(images[x])[0])
+        known_names.append(profiles_list[x]["nome"])
 
 video_capture = cv2.VideoCapture(0)
 
-bruno_image = fr.load_image_file("Rodrigo.jpg")
-bruno_face_encoding = fr.face_encodings(bruno_image)[0]
+path = "Profiles"
+known_face_encondings = []
+known_face_names = []
+collect_imgs(path, known_face_encondings, known_face_names)
 
-known_face_encondings = [bruno_face_encoding]
-known_face_names = ["Rodrigo"]
+
 
 while True:
     ret, frame = video_capture.read()
@@ -22,7 +43,7 @@ while True:
 
         matches = fr.compare_faces(known_face_encondings, face_encoding)
 
-        name = "Unknown"
+        name = "Usuario nao reconhecido"
 
         face_distances = fr.face_distance(known_face_encondings, face_encoding)
 
