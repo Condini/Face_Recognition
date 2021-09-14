6b5ac4d5a53f7f314f4337f6ad8e9fd98cf36909
Condini <guicondini@uol.com.br>
2021-09-14 20:49:47 -0300

    inserindo dados no firebase banco

--- __main__.py
+++ __main__.py
@@ -3,9 +3,24 @@ import face_recognition as fr
 import cv2
 import json
 import os, os.path
+import pyrebase
 from os import listdir
 from os.path import isfile, join
 from datetime import datetime
+
+config = {
+"apiKey": "AIzaSyDzRHZQ_OStJB_eTqP0pK-4eAB42a34XXk",
+"authDomain": "reconhecimentofacial-f73d0.firebaseapp.com",
+"databaseURL": "https://reconhecimentofacial-f73d0-default-rtdb.firebaseio.com",
+"projectId": "reconhecimentofacial-f73d0",
+"storageBucket": "reconhecimentofacial-f73d0.appspot.com",
+"messagingSenderId": "744078029755",
+"appId": "1:744078029755:web:b7901c68ce35fe87444a9e",
+"measurementId": "G-31DJEMKSH5"
+}
+
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
 
 def collect_imgs(path, known_face, known_names):
 
@@ -79,8 +94,16 @@ obj = [
 "Data: " + str(hoje)
 ]
 
+objJson = {
+"Nome: ": str(name),
+"Tempo de Lavagem: ": str(tempoDeLavagem),
+"Data: ": str(hoje)
+}
+
 arquivo = open("logs.txt", "a")
 arquivo.writelines(str(obj) + "\n")
+
+db.child("dados").push(objJson)
 
 #
 
